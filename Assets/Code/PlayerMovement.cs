049d12faae20c4dc14dbca1390a93851fbf32946
Brandon Altamirano <baltamiranoortega@sdsu.edu>
2021-01-08 23:22:51 -0800

    Adding stuff

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animator anim;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+
+    float nextAttackTime = 0f;
+    public float attackRate = 2f;
+    float turnSmoothVelocity;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (horizontal != 0 || vertical != 0)
+        {
+            anim.SetBool("Move", true);
+        }
+
+        else
+        {
+            anim.SetBool("Move", false);
+        }
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            controller.Move(direction * speed * Time.deltaTime);
+            
+        }
+        if (Time.time >= nextAttackTime)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+    void Attack()
+    {
+        Debug.Log("Attack");
+        anim.SetTrigger("Attack");
+        
+    }
+}
