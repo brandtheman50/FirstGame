1a88e520926d13acd5dfe52446ddae7b67bc5351
Brandon Altamirano <baltamiranoortega@sdsu.edu>
2021-01-09 21:56:14 -0800

    Implemented health bar image and slider, health stats still required

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Animator anim;
+
+    public Health healthBar;
+    public int maxHealth = 100;
+    public int currentHealth;
 
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
@@ -15,6 +22,11 @@ public class PlayerMovement : MonoBehaviour
     float turnSmoothVelocity;
     int attack = 0;
 
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -29,7 +41,7 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetBool("Move", false);
         }
-        if(direction.magnitude >= 0.1f && !(Input.GetButton("Fire2")))
+        if(direction.magnitude >= 0.1f && !(Input.GetButton("Fire2")) && !(Input.GetMouseButton(0)))
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -37,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
             controller.Move(direction * speed * Time.deltaTime);
             
         }
-        if (Input.GetButton("Fire2"))
+        if (Input.GetMouseButton(1))
         {
             anim.SetBool("Defend", true);
         }
@@ -45,8 +57,11 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetBool("Defend", false);
         }
-        
-        if (Time.time >= nextAttackTime && !Input.GetButton("Fire2"))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            TakeDamage(20);
+        }
+        if (Time.time >= nextAttackTime && !Input.GetMouseButton(1))
         {
 
             if (Input.GetMouseButton(0))
@@ -66,5 +81,11 @@ public class PlayerMovement : MonoBehaviour
         
         anim.SetTrigger("Attack");
         
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
